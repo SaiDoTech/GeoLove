45d1b7516c9f67fdfcd04966d4078a8a12063bf1
Yaroslav Sidorkevich <72098175+SaiDoTech@users.noreply.github.com>
2021-04-09 11:56:35 +0300

    Log In added

--- XamarinApp/XamarinApp/App.xaml.cs
+++ XamarinApp/XamarinApp/App.xaml.cs
@@ -3,15 +3,21 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamarinApp.View;
 
+using Firebase.Database;
+using XamarinApp.Model;
+
 namespace XamarinApp
 {
     public partial class App : Application
     {
         public static App currentApp { get; private set; }
+        public static FirebaseClient firebaseClient { get; private set; }
 
         public App()
         {
             currentApp = this;
+            firebaseClient = new FirebaseClient("https://geolovemaps-default-rtdb.firebaseio.com/");
+
             MainPage = new LogInPage();
 
             InitializeComponent();
--- XamarinApp/XamarinApp/View/LogInPage.xaml.cs
+++ XamarinApp/XamarinApp/View/LogInPage.xaml.cs
@@ -6,35 +6,58 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Firebase.Database;
-using Firebase.Database.Query;
+
 using XamarinApp.Model;
+using Firebase.Database.Query;
 
 namespace XamarinApp.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LogInPage : ContentPage
     {
-        FirebaseClient firebaseClient;
-
         public LogInPage()
         {
-            firebaseClient = new FirebaseClient("https://geolovemaps-default-rtdb.firebaseio.com/");
-
             InitializeComponent();
         }
 
-        private void OnLogButtonClicked(object sender, EventArgs e)
+        private async void OnLogButtonClicked(object sender, EventArgs e)
         {
-            RegUser();
-            //App.currentApp.GotLogged();
+            //RegUser();
+
+            var userName = loginEntry.Text;
+            var pass = passEntry.Text;
+
+            var response = await CheckUser(userName, pass);
+
+            if (response != null)
+            {
+                App.currentApp.GotLogged();
+            }
+            else
+            {
+                await this.DisplayAlert("Alert","Invalid login or password!","OK");
+            }
         }
 
         async void RegUser()
         {
-            await firebaseClient
+            await App.firebaseClient
                 .Child("Users")
-                .PostAsync(new User(){Id = 0, Login = "user", Password = "123"});
+                .PostAsync(new User(){Id = 2, Login = "user3", Password = "111"});
+        }
+        async Task<User> CheckUser(string userName, string pass)
+        {
+            var gotted = (await App.firebaseClient
+                .Child("Users")
+                .OnceAsync<User>()).Where(a => a.Object.Login == userName).Where(b => b.Object.Password == pass).FirstOrDefault();
+
+            if (gotted != null)
+            {
+                var content = gotted.Object as User;
+                return content;
+            }
+            else
+                return null;
         }
     }
 }
